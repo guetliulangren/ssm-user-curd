933f336e0d5c816ad354236fc6bd21866c0c8979
LGM <1045702116@qq.com>
2018-10-31 15:56:01 +0800

    增加了roles的mapper和修改了shiro配置文件

--- ssm-user-curd/src/main/java/com/controller/Usercontroller.java
+++ ssm-user-curd/src/main/java/com/controller/Usercontroller.java
@@ -1,27 +1,16 @@
 package com.controller;
-
-import org.apache.ibatis.logging.Log;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
-
-import com.lgm.bean.User;
-import com.service.Userservice;
 @Controller
 @RequestMapping("user")
 public class Usercontroller {
-	@Autowired
-    Userservice userservice;
-	
 	//Ó³ÉäÂ·¾¶
-	
 	@RequestMapping(value="/longin")
 	@ResponseBody
     public String login(@RequestParam("Name")String name,@RequestParam("Password") String password
@@ -40,7 +29,7 @@ public class Usercontroller {
     	  
     	   subject.login(token);
     	   if (subject.hasRole("admin")) {
-               return "redirect:/admin/showStudent";
+               return "redirect:/admin/adminpage";
            } else if (subject.hasRole("user")) {
                return "redirect:/teacher/showCourse";
            } else if (subject.hasRole("visit")) {
@@ -55,7 +44,7 @@ public class Usercontroller {
     		
    		 }
 	 
-       return "index2.jsp";
+       return "index2";
 	
 	}
 	
--- ssm-user-curd/src/main/java/com/shiro/Shiro1.java
+++ ssm-user-curd/src/main/java/com/shiro/Shiro1.java
@@ -5,39 +5,27 @@ import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.authc.SimpleAuthenticationInfo;
 import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.realm.AuthenticatingRealm;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.lgm.bean.User;
 import com.service.Userservice;
-public class Shiro1 extends AuthenticatingRealm{
-	//自动注入
-	@Autowired
-    Userservice userservice;
+public class Shiro1 extends AuthorizingRealm{
+
+	@Override
+	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 	@Override
 	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
 		// TODO Auto-generated method stub
-	
-		UsernamePasswordToken user=(UsernamePasswordToken) token;
-		String name=user.getUsername();
-	    char[] password=user.getPassword();
-		/*System.out.println(name);
-		System.out.println(password);*/
-	    String pass=String.valueOf(password);
-	    User u=userservice.getlongin(name, pass);
-	   
-	    System.out.println(u);
-	    if(u==null) {
-		
-	     throw new UnknownAccountException("用户 ");
-			
-		 }
-		
-		Object principal=name;
-		Object credentials=password;
-		String realmName=getName();
-		SimpleAuthenticationInfo info=new SimpleAuthenticationInfo(principal, credentials, realmName);
-		return info;
+		return null;
 	}
-
+	
+	
 }
